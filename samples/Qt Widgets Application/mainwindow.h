f348d73facc73ba1fa4e08f0e6d896cd5d99acd9
jesse.hu <jesse.hu@robosense.cn>
2022-11-06 17:02:17 +0800

    fix:Widgets

--- samples/Qt Widgets Application/mainwindow.h
+++ samples/Qt Widgets Application/mainwindow.h
@@ -4,19 +4,14 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() override;
-
-private:
-    Ui::MainWindow *ui;
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 };
 
 #endif // MAINWINDOW_H
